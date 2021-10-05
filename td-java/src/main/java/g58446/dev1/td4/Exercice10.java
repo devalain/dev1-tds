13669ad092a4bf7e9d6046c05f1633efadb9f144
g58446 <58446@etu.he2b.be>
2021-10-05 15:27:14 +0200

    correction alinesque de la fin du TD4

--- td-java/src/main/java/g58446/dev1/td4/Exercice10.java
+++ td-java/src/main/java/g58446/dev1/td4/Exercice10.java
@@ -5,12 +5,32 @@
  */
 package g58446.dev1.td4;
 
+import java.util.Scanner;
+
 /**
  *
  * @author g58446
  */
 public class Exercice10 {
+
     public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
+        System.out.println("Entrez une série de nombres positifs (entrez -1 pour terminer)");
+        int valeur = clavier.nextInt();
+        int premier = valeur;
+        int dernier = valeur;
+        /** Il faut que je trouve un moyen de redefinir dernier (voir 10bis foireux):
+         *  
+         */
+        int sentinelle = -1;
         
+        while (valeur > sentinelle) { 
+            System.out.println("Entrez un autre nombre: ");
+            valeur = clavier.nextInt();
+            
+        
+        }
     }
 }
+
+// Exercice à terminer
